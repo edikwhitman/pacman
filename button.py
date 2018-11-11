8900e95b6eff4e3d6cd439282f2a663b8570d10d
Edward Whitman <edikwhitman@yandex.ru>
2018-11-11 22:51:42 +0700

    Added text class and changed button class

--- button.py
+++ button.py
@@ -1,23 +1,23 @@
 import pygame
+from text import Text
 
 
 class Button:
-    def __init__(self, w, h, screen, x=0, y=0, color=(0, 0, 0), text='', text_color=(255, 255, 255)):
+    def __init__(self, w, h, screen, x=0, y=0, color=(0, 0, 0), b_text='', text_color=(255, 255, 255)):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.screen = screen
         self.color = color
-        self.text = text
-        self.text_color = text_color
+        self.text = Text(0, 0, b_text, 'Comic Sans MS', 35, text_color, True, False)
         pygame.font.init()
 
     def draw(self):
         pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.w, self.h))
-        font = pygame.font.SysFont('comic Sans MS', 20, True)
-        ts = font.render(self.text, True, self.text_color)
-        self.screen.blit(ts, (self.x, self.y))
+        font = pygame.font.SysFont('comic Sans MS', 40, True)
+        ts = font.render(self.text.text, True, self.text.color)
+        self.screen.blit(ts, ((self.x+self.w//2)-self.text.rect.w//2, (self.y+self.h//2)-self.text.rect.h//2))
 
     def is_clicked(self, x, y):
         if (self.x <= x <= self.x + self.w) and (self.y <= y <= self.y + self.h):
