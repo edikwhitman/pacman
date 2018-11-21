13561175fbdc76581581a2f25feb22a55445320e
Maksim Blinov <tenzm@mail.ru>
2018-11-21 18:46:45 +0300

    Optimization of code in the class 'Pacman' Added 'pacman' motion in example.py

--- pacman.py
+++ pacman.py
@@ -1,40 +1,20 @@
 from character import AnimatedCharacter
-from character import Character
-import pygame
+
 
 class Pacman(AnimatedCharacter):
-
     def __init__(self, x, y, width, height, time=1):
-        self.x = x
-        self.y = y
-        self.img_src = pygame.image.load("./images/entity/pacman_eat.png")
-        self.sprites_cnt = 4
-        self.sprite_width = self.img_src.get_rect().width // self.sprites_cnt
-        self.sprite_height = self.img_src.get_rect().height
-        self.width = width
-        self.height = height
-        self.angle = 0
-        self.sprites = self.splitSprites(self.img_src)
-        self.object = self.sprites[0]
-        self.object_rect = self.sprites[0].get_rect()
-        self.time = time
-        self.work_time = 0
-        self.skip_frame = 0
-        self.frame = 0
-        self.loop = True
-        self.end = False
-        self.setPosition(self.x, self.y)
+        super().__init__(x, y, "./images/entity/pacman_eat.png", 4, width, height, True, time)
         self.anim_status = 0  # 0 - есть, 1 - анимация смерти, 2 - стоять
 
-    def setEatAnimation(self):
+    def set_eat_animation(self):
         self.anim_status = 0
-        self.setAnimation("./images/entity/pacman_eat.png", 4)
+        self.set_animation("./images/entity/pacman_eat.png", 4)
 
-    def setDeathAnimation(self):
+    def set_death_animation(self):
         if not self.anim_status == 1:
             self.anim_status = 1
-            self.setAnimation("./images/entity/pacman_death.png", 11, 3, False)
+            self.set_animation("./images/entity/pacman_death.png", 11, False, 3)
 
-    def setStandAnimation(self):
+    def set_stand_animation(self):
         self.anim_status = 2
-        self.setAnimation("./images/entity/pacman_stand.png", 1)
+        self.set_animation("./images/entity/pacman_stand.png", 1)
