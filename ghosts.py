e15288c428652551ef23a1fe8d37c00429b420b4
Blinov Maksim <tenzm@mail.ru>
2018-11-29 23:08:02 +0300

    Optimized ghost patterns

--- ghosts.py
+++ ghosts.py
@@ -2,37 +2,44 @@ from character import AnimatedCharacter
 
 
 class Ghost(AnimatedCharacter):
-    def __init__(self, x, y, img, width=32, height=32, time=5):
-        super().__init__(x, y, img, 2, width, height, True, time)
-        self.movement_direction = 0  # 0 - стоит на месте, 1 - движется вверх, 2 - вниз, 3 - влево, 4 - вправов
-        self.movement_direction_queue = 0  # 0 - стоит на месте, 1 - движется вверх, 2 - вниз, 3 - влево, 4 - вправов
+    def __init__(self, x, y, name, width=32, height=32, time=5):
+        self.name = name
+        super().__init__(x, y, "./images/entity/ghosts/" + self.name + "_moving.png", 8, width, height, True, time)
+        self.set_moving_up_animation()
+        self.movement_direction = 0  # 0 - стоит на месте, 1 - движется вверх, 2 - вниз, 3 - влево, 4 - вправо
 
-    def set_dir_animation(self, name):  # Поставить нужную картинку в зависимости от направления движения
-        if self.movement_direction == 1:
-            self.set_animation("./images/entity/ghosts/{}_moving_up".png.format(name), 2, True, self.time*3)
-        elif self.movement_direction == 2:
-            self.set_animation("./images/entity/ghosts/{}_moving_down.png".format(name), 2, True, self.time*3)
-        elif self.movement_direction == 3:
-            self.set_animation("./images/entity/ghosts/{}_moving_left.png".format(name), 2, True, self.time*3)
-        elif self.movement_direction == 4:
-            self.set_animation("./images/entity/ghosts/{}_moving_right.png".format(name), 2, True, self.time*3)
+    def set_moving_up_animation(self):
+        self.movement_direction = 1
+        self.set_split_sprites_range(5, 6)
+
+    def set_moving_down_animation(self):
+        self.movement_direction = 2
+        self.set_split_sprites_range(7, 8)
+
+    def set_moving_left_animation(self):
+        self.movement_direction = 3
+        self.set_split_sprites_range(3, 4)
+
+    def set_moving_right_animation(self):
+        self.movement_direction = 4
+        self.set_split_sprites_range(1, 2)
 
 
 class Blinky(Ghost):  # Красный
-    def __init__(self, x, y, img="./images/entity/ghosts/blinky_moving_left.png"):
-        super().__init__(x, y, img)
+    def __init__(self, x, y):
+        super().__init__(x, y, "blinky")
 
 
 class Pinky(Ghost):  # Красный
     def __init__(self, x, y):
-        super().__init__(x, y, "./images/entity/ghosts/pinky_moving_down.png")
+        super().__init__(x, y, "pinky")
 
 
 class Inky(Ghost):  # Голубый
     def __init__(self, x, y):
-        super().__init__(x, y, "./images/entity/ghosts/inky_moving_up.png")
+        super().__init__(x, y, "inky")
 
 
 class Clyde(Ghost):  # Оранжевый
     def __init__(self, x, y):
-        super().__init__(x, y, "./images/entity/ghosts/clyde_moving_up.png")
+        super().__init__(x, y, "clyde")
